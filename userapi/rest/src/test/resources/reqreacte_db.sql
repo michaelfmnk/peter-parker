dcaeb9e13d46383fc0ccf6986271b813f606df27
Michael Fomenko <michael.fmnk@gmail.com>
2020-03-01 16:25:06 +0200

    fixed docker container

--- userapi/rest/src/test/resources/reqreacte_db.sql
+++ userapi/rest/src/test/resources/reqreacte_db.sql
@@ -14,8 +14,8 @@ FROM incidents
 WHERE TRUE;
 
 insert into incidents (document_id, description, plate_number, location, reporter_id)
-VALUES ('IQyi2mo.jpg', 'man is sick', 'AE556677IT', POINT(48.473301, 35.001911)::geometry, 2),
-       ('IQyi2mo.jpg', 'man is really sick', 'AE556699IT', POINT(48.470873, 35.023212)::geometry, 2),
-       ('IQyi2mo.jpg', 'man went out of his mind', 'AE556667IT', POINT(48.458124, 35.060291)::geometry, 2),
-       ('IQyi2mo.jpg', 'wtf??', 'AE556677IT', POINT(48.473301, 35.001911)::geometry, 2),
-       ('IQyi2mo.jpg', 'wat?', 'AE567878IT', POINT(48.473301, 35.001911)::geometry, 2);
+VALUES ('IQyi2mo.jpg', 'man is sick', 'AE556677IT', POINT(48.473301, 35.001911)::geometry, 1),
+       ('IQyi2mo.jpg', 'man is really sick', 'AE556699IT', POINT(48.470873, 35.023212)::geometry, 1),
+       ('IQyi2mo.jpg', 'man went out of his mind', 'AE556667IT', POINT(48.458124, 35.060291)::geometry, 1),
+       ('IQyi2mo.jpg', 'wtf??', 'AE556677IT', POINT(48.473301, 35.001911)::geometry, 1),
+       ('IQyi2mo.jpg', 'wat?', 'AE567878IT', POINT(48.473301, 35.001911)::geometry, 1);
