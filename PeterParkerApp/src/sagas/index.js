b358e7f7591aa6573d5a8e43fb7fd344711822e5
Michael Fomenko <michael.fmnk@gmail.com>
2020-03-22 20:09:59 +0200

    refactoring

--- PeterParkerApp/src/sagas/index.js
+++ PeterParkerApp/src/sagas/index.js
@@ -1,8 +1,10 @@
 import {all, fork} from 'redux-saga/effects';
-import session from "./session";
+import session from './session';
+import logger from './logger';
 
 export function* rootSaga() {
     yield all([
-        fork(session)
+        fork(session),
+        fork(logger),
     ]);
 }
