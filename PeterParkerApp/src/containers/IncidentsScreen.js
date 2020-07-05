024a7d9ab9079f92a30a8518c088f0ce528e0345
MIchael Fomenko <michael.fmnk@gmail.com>
2020-07-05 13:33:05 +0300

    final commit

--- PeterParkerApp/src/containers/IncidentsScreen.js
+++ PeterParkerApp/src/containers/IncidentsScreen.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {ActionSheetIOS, StyleSheet, View} from 'react-native';
 import PropTypes from 'prop-types';
 import {getReportedIncidents} from '../redux/actions/incidents';
 import {Container, Content, List} from 'native-base';
@@ -7,11 +8,32 @@ import Geolocation from '@react-native-community/geolocation';
 import {selectIncidents} from '../redux/selectors/incidents';
 import AppHeader from '../components/AppHeader';
 import IncidentListItem from '../components/IncidentListItem';
+import SegmentedControlTab from "react-native-segmented-control-tab";
+import MapView from "react-native-maps";
+import MapMarker from "react-native-maps/lib/components/MapMarker";
+
+
+const TABS = {
+    LIST: 0,
+    MAP: 1,
+};
 
 class IncidentsScreen extends Component {
 
+    state = {
+        selectedIndex: TABS.LIST,
+        coords: {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0122,
+            longitudeDelta: 0.0221,
+        },
+        incidentSelected: false,
+    };
+
     componentDidMount() {
         this.getIncidentsAround();
+        this.setInitialLocation();
     }
 
     getIncidentsAround() {
@@ -32,28 +54,141 @@ class IncidentsScreen extends Component {
         'Reported': 'Reported ',
     };
 
+    handleTabChange = index => {
+        this.setState({
+            selectedIndex: index,
+            incidentSelected: index === TABS.LIST,
+        });
+    }
+
+    isListViewSelected = () => this.state.selectedIndex === 0;
+    isMapViewSelected = () => this.state.selectedIndex === 1;
+
+    setInitialLocation = () => {
+        Geolocation.getCurrentPosition(position => {
+            this.setState({
+                coords: {
+                    ...this.state.coords,
+                    ...position.coords,
+                }
+            })
+        });
+    }
+
+    createMapIncidentTitle = (incident) => {
+        const input = incident.description;
+        const limit = 30;
+        if (input.length > limit) return input.substring(0, limit) + '...';
+        else return input;
+    }
+
+    onListItemPressed = (incident) => () => {
+        this.setState({
+            selectedIndex: TABS.MAP,
+            coords: {
+                ...this.state.coords,
+                latitude: incident.location.lat,
+                longitude: incident.location.lng,
+            },
+            incidentSelected: true,
+        });
+    }
+
+    onListItemLongPressed = (incident) => () => {
+        ActionSheetIOS.showActionSheetWithOptions({
+            options: ["Cancel", "Mark Resolved", "Delete"],
+            destructiveButtonIndex: 2,
+            cancelButtonIndex: 0
+        }, index => {
+        })
+    }
+
     render() {
         return (
             <Container>
                 <AppHeader title={this.namesMapping[this.props.type]} onPress={this.openIncidentForm}/>
-                <Content>
-                    <List>
-                        {
-                            this.props.incidents.map(incident => (
-                                <IncidentListItem
-                                    incident={incident}
-                                    key={incident.id}
-                                />
-                            ))
-                        }
-                    </List>
-                </Content>
+                <SegmentedControlTab
+                    tabsContainerStyle={{margin: 20, position: 'absolute', top: 100, zIndex: 100}}
+                    values={['List', 'Map']}
+                    selectedIndex={this.state.selectedIndex}
+                    onTabPress={this.handleTabChange}
+                />
+                {
+                    this.isListViewSelected() && (<Content>
+
+
+                            <List>
+                                <View key={-1} style={{height: 40}}>
+                                </View>
+                                {
+                                    this.props.incidents.map(incident => (
+                                        <IncidentListItem
+                                            onPress={this.onListItemPressed(incident)}
+                                            onLongPress={this.onListItemLongPressed(incident)}
+                                            incident={incident}
+                                            key={incident.id}
+                                        />
+                                    ))
+                                }
+
+                            </List>
+
+                        </Content>
+                    )
+                }
+
+                {
+                    this.isMapViewSelected() && (
+                        <MapView
+                            ref={ref => {
+                                this.map = ref;
+                            }}
+                            provider="google"
+                            style={styles.map}
+                            showsBuildings={false}
+                            showsUserLocation={true}
+                            showsMyLocationButton={true}
+                            liteMode={true}
+                            showsCompass={true}
+                            initialRegion={this.state.coords}
+                            onMapReady={() => {
+                                !this.state.incidentSelected && this.props.incidents.length > 0 && this.map.fitToSuppliedMarkers(this.props.incidents.map(it => it.id + ''),
+                                    {
+                                        edgePadding:
+                                            {
+                                                top: 50,
+                                                right: 50,
+                                                bottom: 50,
+                                                left: 50
+                                            }
+
+                                    })
+                            }}>
+                            {
+                                this.props.incidents.map(incident => (
+                                    <MapMarker
+                                        key={incident.id}
+                                        title={this.createMapIncidentTitle(incident)}
+                                        description={incident.id + ''}
+                                        identifier={incident.id + ''}
+                                        coordinate={{latitude: incident.location.lat, longitude: incident.location.lng}}
+                                    />
+                                ))
+                            }
+                        </MapView>
+                    )
+                }
             </Container>
 
         );
     }
 }
 
+const styles = StyleSheet.create({
+    map: {
+        flex: 1,
+    },
+})
 IncidentsScreen.propTypes = {
     navigation: PropTypes.object,
     getReportedIncidents: PropTypes.func,
